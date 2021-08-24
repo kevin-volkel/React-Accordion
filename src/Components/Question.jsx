c72edfec668c455f231d4d0fcbae473043f6d6f7
Kevin <kevinator900@gmail.com>
2021-08-24 14:10:06 -0700

    animations

--- src/Components/Question.jsx
+++ src/Components/Question.jsx
@@ -2,21 +2,29 @@ import React, { useState } from "react";
 
 const Question = ({ id, title, info }) => {
   const [showAnswer, setShowAnswer] = useState(false);
+  const [slide, setSlide] = useState('none')
+
+  const toggleAnswer = () => {
+    setSlide( (!showAnswer) ? 'down' : 'up')
+    if (showAnswer) {
+      setShowAnswer(!showAnswer);
+    } else {
+      setTimeout(() => setShowAnswer((prevValue) => !prevValue), 500);
+    }
+  };
 
   return (
     <div>
       <div className="question">
         <div className="title">
           <h4>{title}</h4>
-          <button className="expand" onClick={() => setShowAnswer(!showAnswer)}>
+          <button className="expand" onClick={toggleAnswer}>
             {showAnswer ? "-" : "+"}
           </button>
         </div>
-        {showAnswer && (
-          <div className="info">
-            <p>{showAnswer ? info : ""}</p>
-          </div>
-        )}
+        <div className={`info ${(slide === 'down') ? 'showing' : (slide === 'up') ? 'hidden' : ''}`}>
+          <p>{showAnswer && info}</p>
+        </div>
       </div>
     </div>
   );
